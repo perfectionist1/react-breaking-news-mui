37e3bd5e8aec55268b21f61e223bd9be2c9828d5
Md. Mostafizur Rahman <dev.mostafizur@gmail.com>
2020-09-28 21:26:45 +0600

    Instead of fetch, I used axios to retrieve data

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import ReactDOM from 'react-dom';
 import Button from '@material-ui/core/Button';
 import './App.css';
 import News from './components/News/News';
+import axios from 'axios';
 
 function App() {
   const[articles, setArticles] = useState([]);
+  // useEffect( () => {
+  //   const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=88eb4e12c97c42569e06cca4f705d490';
+  //   fetch(url)
+  //   .then(res => res.json())
+  //   .then( data => setArticles(data.articles))
+  // }, []);
+  //console.log(articles);
   useEffect( () => {
     const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=88eb4e12c97c42569e06cca4f705d490';
-    fetch(url)
-    .then(res => res.json())
-    .then( data => setArticles(data.articles))
+    axios(url)
+    .then( data =>  setArticles(data.data.articles))
   }, []);
-  //console.log(articles);
   return (
     <div >
       <h2>Articles: {articles.length}</h2>
